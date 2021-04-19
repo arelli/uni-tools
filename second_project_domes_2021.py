ec44303ada684a1cdceb755447b7a4f9e9b05a11
Raf Ellinitakis <nickpap556@gmail.com>
2021-04-19 19:53:14 +0300

    Added one-child deletion.

--- second_project_domes_2021.py
+++ second_project_domes_2021.py
@@ -2,6 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from typing import Optional
+
 
 class BST:
     def __init__(self, length, root):
@@ -68,6 +70,8 @@ class BST:
             if curr_data == key:
                 buffer.append([index, self.tree_array[index][0]])
                 return buffer
+            elif curr_data == None:
+                return -1
             elif key > curr_data:
                 buffer.append([index,self.tree_array[index][0]])
                 index = self.get_right(index)
@@ -94,20 +98,40 @@ class BST:
             self.search_range(self.tree_array[root_index][2], k1, k2)  # recursively call right subtree
 
     def delete_node(self, key):
-        print("helloeoeooeoe")
-        index = self.search_key(key)
-        index = index[1][0]
-        if (index == None):
+        search_index = self.search_key(key) # get the position of the item to be deleted 
+        index = search_index[2][0]  # 2 is the number of items the search returned buffer has. Index is at position [0].
+        parent_index = search_index[1][0]
+        child_value = self.tree_array[index][0]
+        child_left = self.tree_array[index][1]
+        child_right = self.tree_array[index][2]
+        parent_value = self.tree_array[parent_index][0]
+        if index == None:
             return -1  # node not found
 
         # there are three cases:
-        # 1) the node is a leaf --> delete it and update the available list
-        if self.tree_array[index][1] == None and self.tree_array[index][2] == None:
-            self.tree_array[index] = [None,None,None]
+        # 1) the node is a leaf --> delete it and update the available list(WORKS)
+        if child_left is None and child_right is None:
             self.avail_positions.append(index)  # doesn't matter if it is added in the front or not
-            #ALSO DELETE PARENT POINTER TO IT!
+            if child_value > parent_value:  # it is the right child
+                self.tree_array[parent_index][2] = None  # delete pointer to right child
+            elif child_value < parent_value: # it is the left child
+                self.tree_array[parent_index][1] = None # delete left child
+            else:
+                return -1
+            self.tree_array[index] = [None, None, None]  # delete the actual node
+
             return 0
+
         # 2) the node has one child --> connect node's parent with node's child
+        if (child_left is None) and (child_right is not None):  # has only right subtree
+            self.tree_array[index] = self.tree_array[child_right]  # copy the child(of the child) node to the child
+            self.tree_array[child_right] = [None, None, None]  # delete the node
+            self.avail_positions.append(child_right)  # add to the available positions list
+
+        if (child_left is not None) and (child_right is  None):  # has only left subtree
+            self.tree_array[index] = self.tree_array[child_left]  # copy the child(of the child) node to the child
+            self.tree_array[child_left] = [None,None,None]  # delete the node
+            self.avail_positions.append(child_left)
 
         # 3) the node has 2 children --> find inorder successor and replace the node
 
@@ -159,7 +183,7 @@ if __name__ == '__main__':
     new_bst.print2D()
 
 
-    new_bst.delete_node(12)
+    new_bst.delete_node(15)
 
     new_bst.print2D()
 
