37ccc37c510e2747aec4990682c99f8b79c3dc9b
Raf Ellinitakis <nickpap556@gmail.com>
2021-04-20 09:55:34 +0300

    delete() update
    delete works for leafs and one-child nodes. still needs work for nodes with two children

--- second_project_domes_2021.py
+++ second_project_domes_2021.py
@@ -1,11 +1,8 @@
-# This is a sample Python script.
+# Project Domes Second
+# April 2021
+# [year_of_enrollment]030118
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from typing import Optional
-
-
-class BST:
+class BST:  # accepts no double keys!
     def __init__(self, length, root):
         self.tree_array = [[None, None, None] for i in range(length)]
         self.avail_positions = [i for i in range(1, length)]
@@ -31,13 +28,13 @@ class BST:
         if not self.avail_positions:
             print("No more available space to write nodes")
             return -1
-        while not_found:
+        while not_found:  # search for a leaf to hang our new node under
             curr_data = self.get_data(index)
             if curr_data == key:
                 return index  # element already in the tree
 
             elif key > curr_data:
-                if self.get_right(index) == None:
+                if self.get_right(index) == None:  # search a node that has no leaf in our direction
                     self.tree_array[index][2] = self.avail_positions[0]  # connect the new child to its parent node
                     self.tree_array[self.avail_positions[0]] = [key, None, None]  # initialise new Node
                     del self.avail_positions[0]
@@ -60,7 +57,6 @@ class BST:
                 else:
                     index = self.get_left(index)
 
-
     def search_key(self, key):  # key should be an int
         buffer = []  # keep the last two nodes traversed in the form of [index_in_tree_data,data]
         not_found = True
@@ -73,13 +69,13 @@ class BST:
             elif curr_data == None:
                 return -1
             elif key > curr_data:
-                buffer.append([index,self.tree_array[index][0]])
+                buffer.append([index, self.tree_array[index][0]])
                 index = self.get_right(index)
             elif key < curr_data:
-                buffer.append([index,self.tree_array[index][0]])
+                buffer.append([index, self.tree_array[index][0]])
                 index = self.get_left(index)
 
-            if len(buffer) > 2:  # retain up to N parent information
+            if len(buffer) > 2:  # keep up to N parent information
                 buffer.pop(0)  # keep the buffer 3 long
 
             if index == None:
@@ -98,14 +94,19 @@ class BST:
             self.search_range(self.tree_array[root_index][2], k1, k2)  # recursively call right subtree
 
     def delete_node(self, key):
-        search_index = self.search_key(key) # get the position of the item to be deleted 
-        index = search_index[2][0]  # 2 is the number of items the search returned buffer has. Index is at position [0].
-        parent_index = search_index[1][0]
+        search_index = self.search_key(key)  # get the position of the item to be deleted
+        index = search_index[(len(search_index)-1)][0]  # 2 is the number of items the search returned buffer has. Index is at position [0].
+
         child_value = self.tree_array[index][0]
         child_left = self.tree_array[index][1]
         child_right = self.tree_array[index][2]
-        parent_value = self.tree_array[parent_index][0]
-        if index == None:
+        if len(search_index) != 1:
+            parent_index = search_index[1][0]
+            parent_value = self.tree_array[parent_index][0]
+        else:  # means that we are deleting the root, which has no parents
+            parent_value = None
+            parent_index = None
+        if index is None:
             return -1  # node not found
 
         # there are three cases:
@@ -114,26 +115,57 @@ class BST:
             self.avail_positions.append(index)  # doesn't matter if it is added in the front or not
             if child_value > parent_value:  # it is the right child
                 self.tree_array[parent_index][2] = None  # delete pointer to right child
-            elif child_value < parent_value: # it is the left child
-                self.tree_array[parent_index][1] = None # delete left child
+            elif child_value < parent_value:  # it is the left child
+                self.tree_array[parent_index][1] = None  # delete left child
             else:
                 return -1
             self.tree_array[index] = [None, None, None]  # delete the actual node
-
+            self.list_of_data.remove(key)
             return 0
 
         # 2) the node has one child --> connect node's parent with node's child
-        if (child_left is None) and (child_right is not None):  # has only right subtree
+        if child_left is None and child_right is not None:  # has only right subtree
             self.tree_array[index] = self.tree_array[child_right]  # copy the child(of the child) node to the child
             self.tree_array[child_right] = [None, None, None]  # delete the node
             self.avail_positions.append(child_right)  # add to the available positions list
+            self.list_of_data.remove(key)
 
-        if (child_left is not None) and (child_right is  None):  # has only left subtree
+        if child_left is not None and child_right is None:  # has only left subtree
             self.tree_array[index] = self.tree_array[child_left]  # copy the child(of the child) node to the child
-            self.tree_array[child_left] = [None,None,None]  # delete the node
+            self.tree_array[child_left] = [None, None, None]  # delete the node
             self.avail_positions.append(child_left)
+            self.list_of_data.remove(key)
 
         # 3) the node has 2 children --> find inorder successor and replace the node
+        if (child_left is not None) and (child_right is not None):
+            # find the successor node
+            index_of_successor = self.list_of_data.index(key) + 1
+
+            # copy successor data(not node!) to the position of the node we want to delete
+            index_of_successor = self.search_key(self.list_of_data[index_of_successor])  # index in the inorder list
+            index_of_successor = index_of_successor[len(index_of_successor)-1][0]  # translate to index in tree_array
+            self.tree_array[index][0] = self.tree_array[index_of_successor][0]  # index in the real tree array
+
+            # if the successor has children, keep the "pointers" to them
+            # if self.tree_array[index_of_successor][1] is not None or self.tree_array[index_of_successor][2] is not None:
+
+
+            # delete parent pointer to the successor node(right or left)
+            #big IF for testing if it is root that we re deleting
+            # search_index = self.search_key(self.tree_array[index_of_successor][0])
+            # parent_index = search_index[len(search_index)-2][0]
+            # if self.tree_array[parent_index][0] > self.tree_array[index_of_successor][0]:
+            #     self.tree_array[parent_index][1] = None  # delete left child
+            # elif self.tree_array[parent_index][0] < self.tree_array[index_of_successor][0]:
+            #     self.tree_array[parent_index][2] = None  # delete right child
+
+
+            # delete the actual successor node and free it in avail list
+            self.tree_array[index_of_successor] = [None, None, None]
+
+            # do the necessary housekeeping...
+            self.avail_positions.append(index_of_successor)
+            self.list_of_data.remove(key)
 
 
 
@@ -161,29 +193,20 @@ class BST:
         print("Data sorted: " + str(self.list_of_data))
         print("number of nodes:" + str(self.nodes))
 
-    # Press the green button in the gutter to run the script.
-
 
 if __name__ == '__main__':
-    new_bst = BST(15, 5)
+    new_bst = BST(15, 5)  # create a tree with 15 nodes and 5(data) as a root
 
-    new_bst.insert_key(2)
-    new_bst.insert_key(8)
-    new_bst.insert_key(1)
-    new_bst.insert_key(3)
-    new_bst.insert_key(10)
-    new_bst.insert_key(15)
-    new_bst.insert_key(12)
-    new_bst.insert_key(0)
-    new_bst.insert_key(3)
+    for i in [2, 8, 7, 1, 3, 10, 12, 20, 0, 3]:
+        new_bst.insert_key(i)
 
     print("test of the range search:")
     new_bst.search_range(0, 2, 10)
 
     new_bst.print2D()
 
+    new_bst.delete_node(1)
 
-    new_bst.delete_node(15)
 
     new_bst.print2D()
 
