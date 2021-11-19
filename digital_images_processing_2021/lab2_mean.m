01e2ee989ab29316862695556a08402a20be658f
Raf Ellinitakis <nickpap556@gmail.com>
2021-11-19 22:53:19 +0200

    fixed wrong height/width!

--- digital_images_processing_2021/lab2_mean.m
+++ digital_images_processing_2021/lab2_mean.m
@@ -14,10 +14,10 @@ imshow(photo_in)
 prompt = 'What is the kernel dimension you want? ';
 kernel_size = input(prompt)
 
-% pad the image around
 padded_image = padarray(photo_in,kernel_size)  % pad in sides...
 padded_image = padarray(padded_image.',kernel_size)  % pad in the other two sides..
 padded_image = padded_image.'  % restore orientation
+
 figure
 imshow(padded_image)
 
@@ -35,8 +35,8 @@ total = 1;
 % the extra rows and columns the filter kernel can occupy
 extra = (kernel_size-1)/2;
 
-for x = extra+1 : height+ extra+ 1 % +1 to avoid out of bounds accesses
-    for y = extra+1 : width + extra+ 1
+for x = extra+1 : width + extra+ 1 % +1 to avoid out of bounds accesses
+    for y = extra+1 : height + extra+ 1
         % implement the filter kernel. Go from (x-extra,y-extra) which is 
         %the top left kernel pixel, to (x+extra,y+extra), which is the 
         % lowest left pixel.
