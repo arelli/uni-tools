88cd9956c8dd2324c855df60b91314fad1398c4d
Raf Ellinitakis <nickpap556@gmail.com>
2021-12-21 21:05:33 +0200

    removed debug prints

--- digital_images_processing_2021/lab4_image1.m
+++ digital_images_processing_2021/lab4_image1.m
@@ -10,123 +10,49 @@ pause(1)
 
 
 %{
-STEP 1
-Image denoising: we notice that the images are degraded by a
-fairly significant noise. Start by eliminating this noise. Typically, this is 
-done by filtering. For example, linear filtering with a Gaussian or averaging kernel,
-nonlinear filtering of the median type or sequential filtering
-alternating openings and closings. You can test these different
-filtering methods in order to select the one that you think as the
-most appropriate.
+Use a smoothing kernel to remove unwanted noise
 %}
 
+% the smoothing kernel
 h = [1 1 1
     1 1 1
     1 1 1]/20;
 
-
 photo_in_conv = imfilter(photo_in,h,'conv');
-figure;
-imshow(photo_in_conv*1.5);
-title('gaussian');
-pause(1);
 
-%%photo_in_conv = imerode(photo_in,strel('line',10,1));
-%%photo_in_conv = imerode(photo_in,strel('disk',3));
-
-%photo_in_conv = imopen(photo_in_conv,strel('disk',4));  % strel's second argument is angle
+%{
+Open the image to remove part of the noise islands
+%}
 photo_in_conv = imopen(photo_in_conv,strel('line',4,1));  % strel's second argument is angle
 
-figure;
-imshow(photo_in_conv);
-title('Opening');
-pause(1);
-
-
+%{
+Close the image to connect neighbouring islands of signal
+%}
 photo_in_conv = imclose(photo_in_conv,strel('line',6,45));
 photo_in_conv = imclose(photo_in_conv,strel('line',6,-45));
 
-figure
-imshow(photo_in_conv)
-title('Closing')
-pause(1)
-
-
-% EDGE DETECTION
 %{
-STEP 2
-Enhancement of linear structures: can be carried out by
-calculating the morphological gradient or by carrying out a
-filtering by an adapted kernel (Prewitt, Sobel ...).
+Remove the openings with disks of size 9 to 15 from the initial image
+This is mainly used for the background blobs.
 %}
-photo_in_conv = imtophat(photo_in_conv,strel('disk',15));
-photo_in_conv = imtophat(photo_in_conv,strel('disk',14));
-photo_in_conv = imtophat(photo_in_conv,strel('disk',13));
-photo_in_conv = imtophat(photo_in_conv,strel('disk',12));
-photo_in_conv = imtophat(photo_in_conv,strel('disk',11));
-photo_in_conv = imtophat(photo_in_conv,strel('disk',10));
-photo_in_conv = imtophat(photo_in_conv,strel('disk',9));
-photo_in_conv = imtophat(photo_in_conv,strel('disk',8));
+for c = 9:15
+    photo_in_conv = imtophat(photo_in_conv,strel('disk',c));
+end
 
-
-
-%photo_in_conv = imclose(photo_in_conv,strel('disk',5));
-
-figure
-imshow(photo_in_conv)
-title('Tophat')
-pause(1)
-
-
-
-% BINARIZATION
 %{
-STEP 3
-Binarization: a threshold can be determined by the Otsu method
-(have a look at the graythresh() function). Also remember to
-eliminate (as far as possible) small unwanted connected
-components and to fill in the holes.
+Translate the grayscale image to Black/Wite with a threshold obtained
+with the Otsu method through graythresh() function.
 %}
 photo_in_conv = im2bw(photo_in_conv, graythresh(photo_in_conv));
-photo_in_conv = imerode(photo_in_conv,strel('line', 6,1));
-figure
-imshow(photo_in_conv)
-title('binarized')
-pause(1)
 
-
-% SKELETALIZATION
 %{
-STEP 4
-Skeletalization: this step consists in keeping only the "central
-lines" of the related components. You will be able to watch how
-the Matlab bwmorph() function works and use it.
+Extracting the sceleton of the neural axon
 %}
-%photo_in_conv = imdilate(photo_in_conv,strel('disk',12));
-
 photo_in_conv = bwmorph(photo_in_conv,'skel',Inf);
 
 figure
 imshow(photo_in_conv)
-title('skeletonized')
+title('final image(skeleton of neural axon)')
 pause(1)
 
-
-
-%photo_in_conv = 255 * repmat(uint8(photo_in_conv), 1, 1, 3);
-R   = 1;  % Value in range [0, 1]
-G   = 0;
-B   = 0;
-photo_in_conv = cat(3, photo_in_conv * R, photo_in_conv * G, photo_in_conv * B);
-
-figure
-imshow(photo_in_conv)
-title('RGB agen')
-pause(1)
-
-
-figure
-imshow(photo_in_conv+im2double(RGB))
-title('Final image')
-pause(1)
 
